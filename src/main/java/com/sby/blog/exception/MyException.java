4e222afaebef6839119d43315dde3d95425a84df
sgzlyy <sgz1987@vip.qq.com>
2018-09-12 14:40:26 +0800

    集成mybitis、lombok，添加全局异常处理

---
+++ src/main/java/com/sby/blog/exception/MyException.java
@@ -0,0 +1,13 @@
+package com.sby.blog.exception;
+
+/**
+ * @author sby
+ * @Title: MyException
+ * @Description: 我的异常
+ * @date 2018/9/12 14:11
+ */
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
